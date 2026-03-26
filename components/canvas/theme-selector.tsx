3744ba64dedc8fedce6c545325802f848ac8d8e9
Vinay Kumar <vinaydagar514@gmail.com>
2026-03-26 14:59:14 +0530

    feat: implement ThemeSelector component for theme selection

--- components/canvas/theme-selector.tsx
+++ components/canvas/theme-selector.tsx
@@ -1,7 +1,67 @@
-import React from "react";
+"use client";
+import { useCanvas } from "@/context/canvas-context";
+import { parseThemeColors, ThemeType } from "@/lib/themes";
+import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
+import { CheckIcon } from "lucide-react";
 
 const ThemeSelector = () => {
-  return <div></div>;
+  const { themes, theme: currentTheme, setTheme } = useCanvas();
+  return (
+    <div className="flex flex-col max-h-96">
+      <div className="flex-1 pb-2 px-4 overflow-auto">
+        <h3 className="font-semibold text-sm mb-2 "> Choose a theme</h3>
+        <div className="py-2 space-y-3">
+          {themes?.map((theme) => (
+            <ThemeItem
+              key={theme.id}
+              theme={theme}
+              isSelected={currentTheme?.id === theme.id}
+              onSelect={() => setTheme(theme.id)}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
+
+function ThemeItem({
+  theme,
+  isSelected,
+  onSelect,
+}: {
+  theme: ThemeType;
+  isSelected: boolean;
+  onSelect: () => void;
+}) {
+  const color = parseThemeColors(theme.style);
+  return (
+    <Button
+      onClick={onSelect}
+      className={cn(
+        `flex items-center justify-between w-full p-1 rounded-xl border gap-4 bg-background`,
+        isSelected ? "border-2" : "border",
+      )}
+      style={{
+        borderColor: isSelected ? color.primary : "",
+      }}
+    >
+      <div className="flex gap-2">
+        {["primary", "secondary", "accent", "muted"].map((key) => (
+          <div
+            key={key}
+            className="w-4 h-4 rounded-full border"
+            style={{ backgroundColor: color[key], borderColor: "#ccc" }}
+          />
+        ))}
+      </div>
+      <div className="flex items-center gap-2 flex-[0.9]">
+        <span className="text-sm ">{theme.name}</span>
+        {isSelected && <CheckIcon size={16} color={color.primary} />}
+      </div>
+    </Button>
+  );
+}
 
 export default ThemeSelector;
